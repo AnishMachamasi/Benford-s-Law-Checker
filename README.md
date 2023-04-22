7ce88fce543595d8ad241d2af5a553c75c80ac6e
Shamjahan1AnishMachamsai <aneeshmacha11@gmail.com>
2023-04-23 00:23:10 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Brenford's Law Checker
+
+In this project, we have implemented Brenford's Law Checker using Pyramid Framework. 
+
+# Requirements
+- Python
+- Pyramid
+  
+# Installation
+`pip install pyramid=="1.10.5"`
+
+You can visit its official website for more information regarding installation: https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/install.html
+
+To know more about Pyramid Framework, visit: https://trypyramid.com/
+
+## Brenford's Law
+Benford's Law is an observation about the distribution of the frequencies of the first digits of the numbers in many different data sets. It is frequently found that the first digits are not uniformly distributed, but follow the logarithmic distribution
+<p align="center">
+<img src="./1.png">
+</p>
+
+That is, numbers starting with 1 are more common than those starting with 2, and so on, with those starting with 9 the least common. The probabilities are given below:
+<p align="center">
+<img src="./2.png">
+</p>
+
+Benford's Law is most accurate for data sets which span several orders of magnitude, and can be proved to be exact for some infinite sequences of numbers.
---
+++ app.py
@@ -0,0 +1,50 @@
+from pyramid.view import view_config
+from pyramid.config import Configurator
+from wsgiref.simple_server import make_server
+import pandas as pd
+import math
+
+@view_config(route_name='home', renderer='templates/home.jinja2')
+def home(request):
+    return {}
+
+@view_config(route_name='benford', renderer='templates/home.jinja2')
+def benford(request):
+    # Get the uploaded file from the request
+    file = request.POST['file'].file
+
+    # Load the CSV data into a pandas DataFrame
+    data = pd.read_csv(file, header=None, names=['value'])
+
+    # Get the first digit of each value
+    first_digits = data['value'].apply(lambda x: int(str(x)[0]))
+
+    # Calculate the expected Benford distribution
+    benford = [None] + [math.log10((i+1)/i) for i in range(1, 10)]
+
+    # Calculate the actual distribution of first digits
+    actual = first_digits.value_counts(normalize=True).sort_index()
+
+    # Compare the expected and actual distributions
+    diff = abs(pd.Series(benford) - actual).sum()
+
+    # If the difference is within a certain threshold, return success
+    if diff < 0.1:
+        conforms = True
+    else:
+        conforms = False
+        
+    # Render the template with the result included
+    return {'result': {'conforms': conforms, 'probabilities': actual.to_dict()}}
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.include('pyramid_jinja2')
+        config.add_static_view(name='static', path='static')
+        config.add_route('home', '/')
+        config.add_route('benford', '/benford')
+        config.scan()
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    print("Running on http://0.0.0.0:6543")
+    server.serve_forever()
